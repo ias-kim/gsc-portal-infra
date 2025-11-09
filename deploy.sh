c43c00748564a86eb6bb3f564aee8ab85f73331d
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-11-09 22:22:24 +0900

    환경변수 설정 수정

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Starting GSC Portal deployment..."
 
-# 1. 최신 이미지 가져오기
-docker compose -f docker-compose.prod.yml
+# 1. 최신 이미지 가져오기 (pull 추가)
+docker compose -f docker-compose.prod.yml pull
 
-# 2. 컨테이너 재시작
-docker compose -f docker-compose.prod.yml
+# 2. 컨테이너 재시작 (up -d 추가)
+docker compose -f docker-compose.prod.yml up -d
 
 # 3. 불필요한 이미지 정리
 docker image prune -f 
